1eeb7823ed3c54a1e6709fa1fb4d8ec6b1e4b1b1
tcotuleo <t.cotuleo@gmail.com>
2014-08-11 18:45:38 -0400

    Commented everything.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+if (isset($_POST['age'])) {
+    $age = $_POST['age'];
+} else {
+    $age = 30;
+}
+if (isset($_POST['income'])) {
+    $income = $_POST['income'];
+} else {
+    $income = 60000;
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,6 +23,11 @@
 <script language="javascript" type="text/javascript" src="script.js"></script>
     </head>
     <body>
+        <form action="index.php" method="post">
+            <p>Age: <input type="text" name="age" value=<?php echo $age ?> /><br>
+               Income: <input type="text" name="income" value=<?php echo $income ?> /><br>
+            <p><input type="submit" value="Send it!"></p>
+        </form>
         <div class="dataforchart"> <!-- VALUES -->
             <input type="text" value="5">
             <input type="text" value="2">
@@ -23,20 +40,25 @@
         <div id="chartdiv" style="height:300px;width:400px; "></div>
         <script>
             $(document).ready(function() {
-            $('.dataforchart').each(function(){
+                $('.dataforchart').each(function(){
 
-            var points = [];
-            $(this).children("input").each(function(index) {
-                points[index] = $(this).val();
-            });
-            var age = $('input[name=age]').val();
-            var firstLine = 2;
-            $.jqplot('chart1',  [points],{
-                title:'Exponential Line',
-                axes:{yaxis:{min:0, max:240}},
-                series:[{color:'#5FAB78'}]
-            });
-            });
+                    var points = [];
+                    $(this).children("input").each(function(index) {
+                        points[index] = $(this).val();
+                    });
+                    var age = $('input[name=age]').val();
+                    var firstLine = 2;
+                    $.jqplot('chart1',  [[[1, age],[2,15]]],{
+                        title:'Exponential Line',
+                        axes:{yaxis:{min:0, max:240}},
+                        series:[{color:'#5FAB78'}]
+                    });
+//                    $.jqplot('chart1',  [points],{
+//                        title:'Exponential Line',
+//                        axes:{yaxis:{min:0, max:240}},
+//                        series:[{color:'#5FAB78'}]
+//                    });
+                });
             });
         </script>
         <p>Bottom of Chart
