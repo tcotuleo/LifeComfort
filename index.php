66b6e3d60539ee01a6701cefad8178f652c730ae
tcotuleo <t.cotuleo@gmail.com>
2014-08-15 16:05:52 -0400

    Removed point lables. Put fields and graph side by side.

--- index.php
+++ index.php
@@ -63,13 +63,13 @@ for ($year=$age;$year<$age_retirement;$year++){
     }
 }
 $js_array = json_encode($total_by_year);
-print_r($js_array);
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Retirement Calculator</title>
+        <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
 <script language="javascript" type="text/javascript" src="dist/jquery.min.js"></script>
 <script language="javascript" type="text/javascript" src="dist/jquery.jqplot.min.js"></script>
@@ -81,21 +81,22 @@ print_r($js_array);
 <script language="javascript" type="text/javascript" src="script.js"></script>
     </head>
     <body>
-        <form action="index.php" method="post">
-            <p>Age: <input type="text" name="age" value=<?php echo $age ?> /><br>
-               Income: <input type="text" name="income" value=<?php echo $income ?> /><br>
-               Income Change: <input type="text" name="income_change" value=<?php echo $income_change ?> /><br>
-               Percent of Income Contribution: <input type="text" name="percent_income" value=<?php echo $percent_income ?> /><br>
-               Age of Retirement: <input type="text" name="age_retirement" value=<?php echo $age_retirement ?> /><br>
-               Current Savings: <input type="text" name="current_savings" value=<?php echo $current_savings ?> /><br>
-               Interest Rate: <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br>
-               Year of Interest Change: <input type="text" name="year_interest" value=<?php echo $year_interest ?> /><br>
-               New Interest: <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br>
-               Inflation: <input type="text" name="inflation" value=<?php echo $inflation ?> /><br>
-            <p><input type="submit" value="Send it!"></p>
-        </form>
-        <p>Top of Chart
-        <div id="chart1" style="height:450px;width:600px;"> <!-- CHART -->
+        <div>
+            <form action="index.php" method="post">
+                <p>Age: <input type="text" name="age" value=<?php echo $age ?> /><br>
+                   Income: <input type="text" name="income" value=<?php echo $income ?> /><br>
+                   Income Change: <input type="text" name="income_change" value=<?php echo $income_change ?> /><br>
+                   Percent of Income Contribution: <input type="text" name="percent_income" value=<?php echo $percent_income ?> /><br>
+                   Age of Retirement: <input type="text" name="age_retirement" value=<?php echo $age_retirement ?> /><br>
+                   Current Savings: <input type="text" name="current_savings" value=<?php echo $current_savings ?> /><br>
+                   Interest Rate: <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br>
+                   Year of Interest Change: <input type="text" name="year_interest" value=<?php echo $year_interest ?> /><br>
+                   New Interest: <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br>
+                   Inflation: <input type="text" name="inflation" value=<?php echo $inflation ?> /><br>
+                <p><input type="submit" value="Send it!"></p>
+            </form>
+        </div>
+        <div id="chart1" style="height:500px;width:800px;"> <!-- CHART -->
         </div>
         <script>
             var points = <?php echo $js_array ?>;
@@ -103,14 +104,13 @@ print_r($js_array);
             
             { title:'Retirement Calculator',
               animate: true,
-              axes:{xaxis:{min:0},yaxis:{min:0}},
+              axes:{xaxis:{min:1},yaxis:{min:0}},
               series:[{color:'#5FAB78'}],
             seriesDefaults: { 
               showMarker:true,
-              pointLabels: { show:true } 
+              pointLabels: { show:false } 
             }
             });
         </script>
-        <p>Bottom of Chart
     </body>
 </html>
