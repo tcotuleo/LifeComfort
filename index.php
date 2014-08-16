74c89fbe3d55c607af5a0f77ba7f2e475193ac77
tcotuleo <t.cotuleo@gmail.com>
2014-08-16 18:39:26 -0400

    Added greeting.

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ if (isset($_POST['income'])) {
 if (isset($_POST['income_change'])) {
     $income_change = $_POST['income_change'];
 } else {
-    $income_change = .05;
+    $income_change = 50;
 }
-if (isset($_POST['percent_income'])) {
-    $percent_income = $_POST['percent_income'];
+if (isset($_POST['income_contribute'])) {
+    $income_contribute = $_POST['income_contribute'];
 } else {
-    $percent_income = .10;
+    $income_contribute = 10;
 }
 if (isset($_POST['age_retirement'])) {
     $age_retirement = $_POST['age_retirement'];
@@ -32,7 +32,7 @@ if (isset($_POST['current_savings'])) {
 if (isset($_POST['interest_rate'])) {
     $interest_rate = $_POST['interest_rate'];
 } else {
-    $interest_rate = .065;
+    $interest_rate = 6.5;
 }
 if (isset($_POST['year_interest'])) {
     $year_interest = $_POST['year_interest'];
@@ -42,24 +42,25 @@ if (isset($_POST['year_interest'])) {
 if (isset($_POST['new_interest'])) {
     $new_interest = $_POST['new_interest'];
 } else {
-    $new_interest = .07;
+    $new_interest = 7;
 }
 if (isset($_POST['inflation'])) {
     $inflation = $_POST['inflation'];
 } else {
-    $inflation = .04;
+    $inflation = 4;
 }
+
 $total_by_year = [];
 $total = $current_savings;
 $income_current = $income;
 array_push($total_by_year,$total);
 
 for ($year=$age;$year<$age_retirement;$year++){
-    $total = round(($total * (1 + $interest_rate)) + ($income * $percent_income),2);
+    $total = round(($total * (1 + ($interest_rate/100))) + ($income * ($income_contribute/100)),2);
     array_push($total_by_year,$total);
-    $income_current = $income_current * (1 + $income_change);
+    $income_current = $income_current * (1 + ($income_change/100));
     if ($year >= $year_interest){
-        $interest = $new_interest;
+        $interest = ($new_interest/100);
     }
 }
 $js_array = json_encode($total_by_year);
@@ -81,26 +82,28 @@ $js_array = json_encode($total_by_year);
 <script language="javascript" type="text/javascript" src="script.js"></script>
     </head>
     <body>
+        <h3 id="greeting"> Thank you for choosing LifeComfort. This calculator<br>
+            can be used to make a comfortable retirement plan.</h3><br>
         <div id="values">
             <p id="values_title">Values
             <form action="index.php" method="post">
                 <p>Age: <input type="text" name="age" value=<?php echo $age ?> /><br>
                    Income: <input type="text" name="income" value=<?php echo $income ?> /><br>
-                   Income Change: <input type="text" name="income_change" value=<?php echo $income_change ?> /><br>
-                   Percent of Income Contribution: <input type="text" name="percent_income" value=<?php echo $percent_income ?> /><br>
+                   Income Change %: <input type="text" name="income_change" value=<?php echo $income_change ?> /><br>
+                   Income Contribution %: <input type="text" name="income_contribute" value=<?php echo $income_contribute ?> /><br>
                    Age of Retirement: <input type="text" name="age_retirement" value=<?php echo $age_retirement ?> /><br>
                    Current Savings: <input type="text" name="current_savings" value=<?php echo $current_savings ?> /><br>
-                   Interest Rate: <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br>
+                   Interest Rate %: <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br>
                    Year of Interest Change: <input type="text" name="year_interest" value=<?php echo $year_interest ?> /><br>
-                   New Interest: <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br>
-                   Inflation: <input type="text" name="inflation" value=<?php echo $inflation ?> /><br>
-                <p><input type="submit" value="Send it!"></p>
+                   New Interest %: <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br>
+                   Inflation %: <input type="text" name="inflation" value=<?php echo $inflation ?> /><br>
+                <p><input type="submit" value="Calculate">
             </form>
         </div>
         <div id="chart1" style="height:500px;width:800px;"> <!-- CHART -->
         </div>
         <div id="message">
-        <?php echo "By age " . $age_retirement . " you will have $" . end($total_by_year) . "." ?>
+        <?php echo "<h2>By age " . $age_retirement . " you will have $" . end($total_by_year) . ".</h2>" ?>
         </div>
         <script>
             var points = <?php echo $js_array ?>;
@@ -112,7 +115,7 @@ $js_array = json_encode($total_by_year);
             seriesDefaults: { 
               showMarker:true,
               pointLabels: { show:false },
-              shadow: true,   // show shadow or not.
+              shadow: true   // show shadow or not.
             }
             });
         </script>
