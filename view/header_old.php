ba96483c9bd01a82c7ecca94062d5777df1f78d0
tcotuleo <t.cotuleo@gmail.com>
2014-10-25 17:45:52 -0400

    Changed chart from JQPlot to Google Chart.

---
+++ view/header_old.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $page_title ?></title>
+        <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+        <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
+        <script language="javascript" type="text/javascript" src="dist/jquery.min.js"></script>
+        <script language="javascript" type="text/javascript" src="dist/jquery.jqplot.min.js"></script>
+        <script type="text/javascript" src="dist/plugins/jqplot.pointLabels.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="dist/jquery.jqplot.css" />
+    </head>
+    <body>
+        <?php
+        
+
+?>
+        <h3 id = "greeting"> Thank you for choosing LifeComfort. This calculator can be used to make a comfortable retirement plan.</h3><br>
+        <p align="middle"> <img src="view/logo.jpg" width="250" height="200" ></p>
+            
--- website_old.php
+++ website_old.php
@@ -115,18 +115,15 @@
         $inflation = 4;
     }
 
-    $chart_array = [];
+    $total_by_year = [];
+    $inflation_by_year = [];
 
     if (!$non_number) {
-        $total = floatval($current_savings);
-        $inflation_total = floatval($current_savings);
+        $total = $current_savings;
+        $inflation_total = $current_savings;
         $income_current = $income;
-        
-        $title = array("Age", "Total", "Adjusted for inflation");
-        $chart_entry = array(strval($age), $total, $inflation_total);
-        array_push($chart_array, $title);
-        array_push($chart_array,$chart_entry);
-        
+        array_push($total_by_year,$total);
+        array_push($inflation_by_year,$inflation_total);
         $interest_rate_effective = $interest_rate;
         
         for ($year=$age;$year<$age_retirement;$year++){
@@ -137,69 +134,64 @@
             
             $total = round(($total * (1 + ($interest_rate_effective/100))) + ($income_current * ($income_contribute/100)),2);
             $inflation_total = round((($inflation_total * (1 + ($interest_rate_effective/100))) + ($income_current * ($income_contribute/100)))*(1-($inflation/100)),2);
-            $chart_entry = array(strval($year+1), $total, $inflation_total);
-            array_push($chart_array,$chart_entry);
+            array_push($total_by_year,$total);
+            array_push($inflation_by_year,$inflation_total);
             $income_current = $income_current * (1 + ($income_change/100));
-            $last_total=$total;
-            $last_inflation=$inflation_total;
         }
     }
     else{
-        array_push($chart_array, 0, 0);
+        array_push($total_by_year,0);
+        array_push($inflation_by_year,0);
     }
     
-    $chart_array_json = json_encode($chart_array);
+    $total_array = json_encode($total_by_year);
+    $inflation_array = json_encode($inflation_by_year);
 ?>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      var chart_array = <?php echo $chart_array_json ?>;
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable(chart_array);
-
-        var options = {
-          title: 'Retirement Savings',
-          animation:{
-            duration: 1000,
-            easing: 'out',
-          }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-        chart.draw(data, options);
-      }
-    </script>
-    
     <!--Display the values form -->
     <div id="values">
         <table align='center'> 
             <p id="values_title">Values </p>
-            <form action='website.php' method='post'>
-                <tr> <td>Age: </td><td> <input type="text" name="age" value=<?php echo $age ?> /><br> </td></tr>
-                <tr> <td>Income: </td><td> <input type="text" name="income" value=<?php echo $income ?> /><br> </td></tr>
-                <tr> <td>Income Change %: </td><td> <input type="text" name="income_change" value=<?php echo $income_change ?> /><br> </td></tr>
-                <tr> <td>Income Contribution %: </td><td> <input type="text" name="income_contribute" value=<?php echo $income_contribute ?> /><br> </td></tr>
-                <tr> <td>Age of Retirement: </td><td> <input type="text" name="age_retirement" value=<?php echo $age_retirement ?> /><br> </td></tr>
-                <tr> <td>Current Savings: </td><td> <input type="text" name="current_savings" value=<?php echo $current_savings ?> /><br> </td></tr>
-                <tr> <td>Interest Rate %: </td><td> <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br> </td></tr>
-                <tr> <td>Year of Interest Change: </td><td> <input type="text" name="year_interest" value=<?php echo $year_interest ?> /><br> </td></tr>
-                <tr> <td>New Interest %: </td><td> <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br> </td></tr>
-                <tr> <td>Inflation %: </td><td> <input type="text" name="inflation" value=<?php echo $inflation ?> /><br></td></tr>
-                <tr> <td><input id='button' type="submit" value="Calculate"></td> </tr>
-            </form>
+                <form action='website.php' method='post'>
+                    <tr> <td>Age: </td><td> <input type="text" name="age" value=<?php echo $age ?> /><br> </td></tr>
+                    <tr> <td>Income: </td><td> <input type="text" name="income" value=<?php echo $income ?> /><br> </td></tr>
+                    <tr> <td>Income Change %: </td><td> <input type="text" name="income_change" value=<?php echo $income_change ?> /><br> </td></tr>
+                    <tr> <td>Income Contribution %: </td><td> <input type="text" name="income_contribute" value=<?php echo $income_contribute ?> /><br> </td></tr>
+                    <tr> <td>Age of Retirement: </td><td> <input type="text" name="age_retirement" value=<?php echo $age_retirement ?> /><br> </td></tr>
+                    <tr> <td>Current Savings: </td><td> <input type="text" name="current_savings" value=<?php echo $current_savings ?> /><br> </td></tr>
+                    <tr> <td>Interest Rate %: </td><td> <input type="text" name="interest_rate" value=<?php echo $interest_rate ?> /><br> </td></tr>
+                    <tr> <td>Year of Interest Change: </td><td> <input type="text" name="year_interest" value=<?php echo $year_interest ?> /><br> </td></tr>
+                    <tr> <td>New Interest %: </td><td> <input type="text" name="new_interest" value=<?php echo $new_interest ?> /><br> </td></tr>
+                    <tr> <td>Inflation %: </td><td> <input type="text" name="inflation" value=<?php echo $inflation ?> /><br></td></tr>
+                    <tr> <td><input id='button' type="submit" value="Calculate"></td> </tr>
+                </form>
         </table>
     </div>
-        
+    
     <!--Display the chart -->
-    <div id="chart_div" style="width: 900px; height: 500px;"></div>
-
+    <div id="chart1" style="height:450px;width:600px;"></div>
+    
     <!--Display the summary message -->
     <div id="message">
-        <?php echo "<h3>By age " . $age_retirement . " you will have $" . $last_total . ".</h3>";
-              echo "<p><h3>That is $" . $last_inflation . " when adjusted for " . $inflation . "% inflation.</h3>"?>
+        <?php echo "<h3>By age " . $age_retirement . " you will have $" . end($total_by_year) . ".</h3>";
+              echo "<p><h3>That is $" . end($inflation_by_year) . " when adjusted for " . $inflation . "% inflation.</h3>"?>
     </div>
+    
+    <script>
+        var total = <?php echo $total_array ?>;
+        var inflation = <?php echo $inflation_array ?>;
+        var plot1 = $.jqplot('chart1',[total,inflation],{ 
+            title:'Retirement Calculator',
+            animate: true,
+            axes:{xaxis:{min:0},yaxis:{min:0}},
+            series:[{color:'#5FAB78'}],
+            seriesDefaults:{ 
+                showMarker:true,
+                pointLabels: { show:false },
+                shadow: true   // show shadow or not.
+            }
+        });
+    </script>
+ 
 <?php
     include "view/footer.php";
 ?>
