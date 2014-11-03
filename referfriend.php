48eb5bfa0d51efc354bb0178dde462150c12e466
tcotuleo <t.cotuleo@gmail.com>
2014-11-02 22:23:57 -0500

    Added smptmail.

--- referfriend.php
+++ referfriend.php
@@ -1,38 +1,35 @@
 
 <?php 
 
- $recipients = 'jakeshpatel@gmail.com';
-
-    $headers['From']    = 'rushil.patel92@gmail.com';
-    $headers['To']      = 'jakeshpatel@gmail.com';
-    $headers['Subject'] = 'Test message';
-
-    $body = 'Test message';
-
-    $smtpinfo["host"] = "smtp.gmail.com";
-    $smtpinfo["port"] = "465";
-    $smtpinfo["auth"] = true;
-    $smtpinfo["username"] = "rushil.patel92";
-    $smtpinfo["password"] = "Rushil@1992";
-
-
-    // Create the mail object using the Mail::factory method
-    $mail_object =& Mail::factory("smtp", $smtpinfo); 
-
-    $mail_object->send($recipients, $headers, $body);
-//$to = "rushil.patel92@gmail.com"; 
-//$subject = "Hi!"; 
-//$body = "Hi,\n\nHow are you?"; 
+// $recipients = 'jakeshpatel@gmail.com';
 //
-//if (mail($to, $subject, $bo dy)) 
-//{   
-//    echo("<p>Email successfully sent!</p>");  
-//    
-//} 
-//else 
-//    {   
-//    echo("<p>Email delivery failed…</p>");  
-//    
-//    } 
-    
-    ?>
+//    $headers['From']    = 'rushil.patel92@gmail.com';
+//    $headers['To']      = 'jakeshpatel@gmail.com';
+//    $headers['Subject'] = 'Test message';
+//
+//    $body = 'Test message';
+//
+//    $smtpinfo["host"] = "smtp.gmail.com";
+//    $smtpinfo["port"] = "465";
+//    $smtpinfo["auth"] = true;
+//    $smtpinfo["username"] = "rushil.patel92";
+//    $smtpinfo["password"] = "Rushil@1992";
+//
+//
+//    // Create the mail object using the Mail::factory method
+//    $mail_object =& Mail::factory("smtp", $smtpinfo); 
+//
+//    $mail_object->send($recipients, $headers, $body);
+$to = "t.cotuleo@gmail.com"; 
+$subject = "Hi!"; 
+$body = "Hi,\n\nHow are you?"; 
+
+if (mail($to, $subject, $body)) 
+{   
+    echo("<p>Email successfully sent!</p>");     
+} 
+else 
+{   
+echo("<p>Email delivery failed…</p>");  
+} 
+?>
